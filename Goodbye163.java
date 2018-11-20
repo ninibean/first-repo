694c2908cb8326018fe588626904c37808f7c3e2
ninibean <40003978+ninibean@users.noreply.github.com>
2018-11-20 03:42:43 -0500

    Update Goodbye163.java

---
+++ Goodbye163.java
@@ -0,0 +1,9 @@
+/**
+ * Simple code that says goodbye to Comp163.
+ * @author nspatterson@aggies.ncat.edu
+ */
+public class Goodbye163 {
+  public static void main(String[] args) {
+  System.out.println("Goodbye COMP 163.");
+  }
+}
